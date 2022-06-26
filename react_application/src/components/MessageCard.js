a070cbcf1ce94adc795632eefc9fc85dc63e9b31
deepakchoudhary358 <deepakchoudhary358@gmail.com>
2022-06-26 20:06:33 +0530

    Message component

---
+++ react_application/src/components/MessageCard.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+let MessageCard = () => {
+    return(
+        <React.Fragment>
+            <div className="card">
+                <div className="card-body">
+                    <h2>Welcome to Message card</h2>
+                    
+                </div>
+            </div>
+            
+        </React.Fragment>
+    );
+}
+
+export default MessageCard;
