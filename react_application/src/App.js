2b5e9b58ca862fa935931357e94b5686ebac02d6
deepakchoudhary358 <deepakchoudhary358@gmail.com>
2022-06-28 11:09:13 +0530

    usesState

--- react_application/src/App.js
+++ react_application/src/App.js
@@ -1,9 +1,11 @@
 import React, { Fragment } from "react";
 import './App.css';
+//import Employee from "./components/Employee";
+import Teacher from "./components/Teacher";
 // import MessageCard from "./components/MessageCard";
 // import WishCard from "./components/WishCard";
-import Employee from "./components/Employee";
-import Student from "./components/Student";
+// import Employee from "./components/Employee";
+// import Student from "./components/Student";
 
 function App() {
   return (
@@ -11,8 +13,10 @@ function App() {
       <nav className="navbar">
         <span> Root Component</span>
       </nav>
-      <Student name = "John" age = "29"/>
-      <Student name = "Jonny" age = "31"/>
+      {/* <Student name = "John" age = "29"/>
+      <Student name = "Jonny" age = "31"/> */}
+      <Teacher />
+
     </Fragment>
   );
 }
